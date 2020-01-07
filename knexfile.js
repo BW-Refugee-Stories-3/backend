adc51f9b9171ce9600c0238cfdc94eb66cc768a8
Your Name <you@example.com>
2020-01-07 13:27:51 -0800

    knexfile - production

--- knexfile.js
+++ knexfile.js
@@ -19,5 +19,12 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
 
 };
