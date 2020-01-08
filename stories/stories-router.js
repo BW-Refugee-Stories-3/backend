b22ab940ec5325e352a9d077f4ef6d8591004ea6
Your Name <you@example.com>
2020-01-07 16:31:52 -0800

    endpoints working

--- stories/stories-router.js
+++ stories/stories-router.js
@@ -1,36 +1,88 @@
-// const router = require('express').Router();
+const router = require('express').Router();
 
-// const Story = require('./stories-model')
+const Story = require('./stories-model.js')
 
-// const restricted = require('../auth/auth-middleware.js');
+const restricted = require('../auth/auth-middleware.js');
 
-// router.get('/approved', (req, res) => {
-    
-// })
+router.get('/approved', (req, res) => {
+    Story.findBy(approved = 0)
+    .then(data => {
+        res.status(200).json(data)
+    })
+    .catch(err => res.send(err));
 
-// router.post('/process', (req, res) => {
-//     let story = req.body;
+})
 
-//     Story.add(story)
-//         .then(data => {
-//             res.status(201).json(data)
-//         })
-//         .catch(err => {
-//             res.status(500).json(err)
-//         })
-// })
+router.post('/process', (req, res) => {
+    let story = req.body;
 
-// router.get('/process', restricted, (req, res) => {
-// })
+    Story.add(story)
+        .then(data => {
+            res.status(201).json(data)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
 
-// router.get('/process/:id', restricted, (req, res) => {
+router.get('/process', (req, res) => {
+    Story.findBy(approved = 1)
+    .then(data => {
+        res.status(200).json(data)
+    })
+    .catch(err => res.send(err));
+})
 
-// })
+router.get('/process/:id', (req, res) => {
+const id = req.params.id
+if (!id) {
+    res.status(404).json({ message: "Could not find story with that ID"})
+} else {
+    Story.findById(id)
+    .then(data => {
+        res.status(201).json(data)
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'The story could not be retrieved.' });
+      })
+    }
+})
 
-// router.delete('/process/:id', restricted, (req, res) => {
+router.delete('/process/:id', (req, res) => {
+    const { id } = req.params;
 
-// })
+    Story.remove(id)
+    .then(data => {
+        if (data) {
+            res.json({deleted})
+        } else {
+            res.status(404).json({ message: 'Could not find log with given id' });
+        }})
+        .catch(err => {
+            res.status(500).json({ 
+              message: 'Failed to update log',
+            });
+          })
+    }) 
 
-// router.put('/process/:id', restricted, (req, res) => {
+router.put('/process/:id', (req, res) => {
+    const changes = req.body
+    const { id } = req.params
 
-// })
+    Story.findById(id)
+    .then(data => {
+        if(data) {
+        Story.update(id, changes)
+            .then(updatedStory => {
+                res.status(200).json(updatedStory);
+             })
+        }    else {
+                res.status(404).json({ message: 'Could not find log with given id' });
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: err})
+    })
+})
+
+module.exports = router
