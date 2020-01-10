2ed8ff2903d3d518efe4cc99a306d0244f639dce
Your Name <you@example.com>
2020-01-09 16:29:35 -0800

    DB changes

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -8,7 +8,7 @@ module.exports = {
 };
 
 function find() {
-  return db('users').select('id', 'username', 'firstName', 'lastName', 'email');
+  return db('users').select('id', 'username');
 }
 
 function findBy(filter) {
@@ -22,7 +22,7 @@ async function add(user) {
 
 function findById(id) {
   return db('users')
-    .select('id', 'username', 'firstName', 'lastName', 'email')
+    .select('id', 'username')
     .where({ id })
     .first();
 }
